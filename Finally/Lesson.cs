df93bebb58efe539e7e2f331822c9fd8c29cbb35
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2018-11-13 21:18:54 +0200

    Lesson

---
+++ Finally/Lesson.cs
@@ -0,0 +1,15 @@
+﻿namespace Finally
+{
+    public class Lesson
+    {
+        public string Name { get; }
+
+        public byte Grade { get; }
+
+        public Lesson(string name, byte grade)
+        {
+            Name = name;
+            Grade = grade;
+        }
+    }
+}
