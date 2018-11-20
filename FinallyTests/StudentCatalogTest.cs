f63b0c8b6be704639e1213d9f91bcd25ac952da6
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2018-11-20 21:50:03 +0200

    Added student catalog enumeration test

--- FinallyTests/StudentCatalogTest.cs
+++ FinallyTests/StudentCatalogTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Finally;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -37,5 +38,28 @@ namespace FinallyTests
             Assert.AreEqual(0, catalog.Count);
         }
 
+        [TestMethod]
+        public void TestStudentCatalogEnumeration()
+        {
+            var catalog = new StudentCatalog();
+            var test = new Student("test", 123);
+            var mary = new Student("mary", 45);
+            catalog.Add(test);
+            catalog.Add(mary);
+
+            var expected = new Dictionary<Student, int>();
+            expected.Add(test, 0);
+            expected.Add(mary, 0);
+
+            foreach (var student in catalog)
+            {
+                expected[student]++;
+            }
+
+            foreach (var actual in expected)
+            {
+                Assert.AreEqual(actual.Value, 1);
+            }
+        }
     }
 }
